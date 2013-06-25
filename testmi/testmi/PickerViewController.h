a2f3b99c999975c778d63d295922bca486041e0e
danqing2k <danqing2k@hotmail.com>
2013-06-25 08:15:37 +0200

    picker & animation

--- testmi/testmi/PickerViewController.h
+++ testmi/testmi/PickerViewController.h
@@ -22,8 +22,7 @@
 - (IBAction)SelectAction:(id)sender;
 - (IBAction)SpeicherAction:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *speicher;
-//@property (weak, nonatomic) IBOutlet UILabel *FachField;
-//@property (weak, nonatomic) IBOutlet UILabel *SemesterField;
+
 
 @property (strong, nonatomic) UIWindow *window;
 @property (weak, nonatomic) IBOutlet UITextField *FachField;
